fa193512910269e175b91e4df0cbfb50d05ac113
Tad Scritchfield <tadscritch@gmail.com>
2018-07-29 10:20:06 -0700

    Add guest api functions

--- src/api/guests.js
+++ src/api/guests.js
@@ -3,3 +3,11 @@ const uri = path => `http://localhost:3001/${path}`
 export const findGuestByName = name => {
   return fetch(uri(`guest/${name}`))
 }
+
+export const rsvpGuest = id => {
+  return fetch(uri(`guest/${id}/rsvp`))
+}
+
+export const reserveSeatsForGuest = id => {
+  return fetch(uri(`guest/${id}/reserve-seats`))
+}
